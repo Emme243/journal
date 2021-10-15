9d6207658d2d23a225ecf8fc1f5e8bee2e8adfb5
Emmanuel <emmlop243@gmail.com>
2021-10-15 14:15:13 -0500

    Maquetación inicial de un sidebar para el daybook (EntryList.vue) y de un componente individual por cada entrada (Entry.vue)

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="entry-container pointer p-2">
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold">15</span>
+      <span class="mx-1 fs-5">Julio</span>
+      <span class="mx-2 fw-light">2021, jueves</span>
+    </div>
+
+    <div class="entry-description">
+      Id enim id elit velit occaecat culpa aliquip do ullamco dolore mollit velit eu. Irure deserunt
+      consectetur enim ad incididunt quis et. Deserunt aliqua proident consequat eiusmod voluptate
+      culpa eiusmod in nisi ad commodo. Minim cupidatat excepteur ut esse ullamco. Est magna
+      pariatur elit ex tempor amet proident sunt nulla ullamco incididunt eu enim. Mollit cillum
+      laborum adipisicing ut quis duis eiusmod quis tempor. Mollit pariatur aliqua aliquip Lorem
+      esse proident excepteur est proident veniam eu tempor.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.entry-container {
+  border-bottom: 1px solid #2c3e50;
+  transition: 0.2s all ease-in;
+
+  &:hover {
+    background-color: lighten($color: grey, $amount: 45);
+    transition: 0.2s all ease-in;
+  }
+
+  .entry-description {
+    font-size: 12px;
+  }
+}
+</style>
--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,7 +1,32 @@
 <template>
-  <h1>EntryList</h1>
+  <div class="entry-list-container">
+    <div class="px-2 pt-2">
+      <input type="text" class="form-control" placeholder="Search Entry" />
+    </div>
+
+    <div class="entry-scrollarea">
+      <Entry v-for="item in 100" :key="item"></Entry>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    Entry: defineAsyncComponent(() => import('./Entry.vue'))
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.entry-list-container {
+  border-right: 1px solid #2c3e50;
+  height: calc(100vh - 56px);
+}
+.entry-scrollarea {
+  height: calc(100vh - 115px);
+  overflow: scroll;
+}
+</style>
