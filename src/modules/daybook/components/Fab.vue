3487b2d59d33707d8c2a1790ab7eefe41c3e7462
Emmanuel <emmlop243@gmail.com>
2021-10-15 14:29:36 -0500

    Componente NoEntrySelected creado, para mostrar que no hay entradas y colocación de un Fab para añadir nuevas entradas

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="btn btn-primary">
+    <i class="fa fa-plus"></i>
+  </button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+}
+</style>
--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -2,5 +2,12 @@ export default {
   name: 'daybook',
   component: () =>
     import(/* webpackChunkName: "daybook" */ '@/modules/daybook/layouts/DayBookLayout'),
-  children: []
+  children: [
+    {
+      path: '',
+      name: 'daybook-no-entry',
+      component: () =>
+        import(/* webpackChunkName: "daybook-no-entry" */ '@/modules/daybook/views/NoEntrySelected')
+    }
+  ]
 }
