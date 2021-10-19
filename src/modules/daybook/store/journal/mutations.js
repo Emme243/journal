d3b6e97c355b8154838fe1098fb13acb69532698
Emmanuel <emmlop243@gmail.com>
2021-10-19 13:32:33 -0500

    Se traen entradas desde la base de datos y son pintadas en pantalla

--- src/modules/daybook/store/journal/mutations.js
+++ src/modules/daybook/store/journal/mutations.js
@@ -1,5 +1,9 @@
 // export const myMutation = (state, payload) => {}
 
-export const setEntries = (/* state, payload */) => {}
+export const setEntries = (state, payload) => {
+  state.entries = [...state.entries, ...payload]
+  state.isLoading = false
+}
+
 export const updateEntry = (/* state, payload */) => {}
 export const addEntry = (/* state, payload */) => {}
--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,26 +1,4 @@
 export default () => ({
   isLoading: true,
-  entries: [
-    {
-      id: '1',
-      date: new Date().toDateString(),
-      text:
-        'Aliquip irure magna sint eu fugiat excepteur esse officia ullamco culpa incididunt id consequat exercitation. Incididunt sint eiusmod laboris eiusmod pariatur. Sunt pariatur duis pariatur fugiat est in eu pariatur sunt consequat sint ex magna.',
-      picture: null
-    },
-    {
-      id: '2',
-      date: new Date().toDateString(),
-      text:
-        'Cupidatat cillum ipsum labore ex. Eiusmod non non veniam in do excepteur labore labore. Consectetur nulla consectetur laboris ea eu nulla in. Quis anim eu non reprehenderit exercitation laborum elit. Adipisicing veniam occaecat commodo elit tempor. Ad fugiat cupidatat incididunt qui nisi anim officia consectetur non officia do Lorem.',
-      picture: null
-    },
-    {
-      id: '3',
-      date: new Date().toDateString(),
-      text:
-        'Excepteur occaecat fugiat fugiat dolor. Aliquip cillum mollit do cupidatat Lorem. Consectetur exercitation labore velit amet Lorem ut consectetur pariatur voluptate minim labore veniam sint. Proident mollit labore in ipsum. Reprehenderit dolor qui pariatur pariatur irure consequat irure voluptate anim amet non quis.',
-      picture: null
-    }
-  ]
+  entries: []
 })
